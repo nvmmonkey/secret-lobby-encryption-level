147ccaa26c44cce3d7c4e3c00333f9fe8a61bdf4
nvmmonkey <nvmmonkey@github.com>
2023-01-19 03:56:36 +0800

    Level 1 - Username and Password Only

---
+++ app.js
@@ -0,0 +1,75 @@
+// IMPORT ////
+const express = require("express");
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+// SETUP ////
+mongoose.set("strictQuery",false)
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true}, ()=>{
+    console.log("Connected to MongoDB ")
+})
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+// Schema ////
+const userSchema = {
+    email: String,
+    password: String
+}
+const User = new mongoose.model("User", userSchema)
+
+// APP GET/POST ////
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+app.get("/login", function(req, res){
+    res.render("login")
+})
+
+app.get("/register", function(req, res){
+    res.render("register")
+})
+
+app.post("/login", function(req, res){
+    const username = req.body.username
+    const password = req.body.password
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets")
+                } else {
+                    res.send("Password is wrong!")
+                }
+            } else {
+                res.send("Can't find a user!")
+            }
+        }
+    })
+})
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        }else{
+            res.render("secrets")
+        }
+    })
+})
+
+// LISTEN ////
+app.listen(3000, ()=>{
+    console.log("Server started on port 3000!")
+})

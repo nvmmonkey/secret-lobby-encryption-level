fc33302681379a4d63596f973adebaca2f6c4f2e
nvmmonkey <nvmmonkey@github.com>
2023-01-20 03:18:37 +0800

    Level 4 - Salting and Hashing Password with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 
 // SETUP ////
@@ -44,36 +45,42 @@ app.get("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
     User.findOne({email: username}, function(err, foundUser){
         if(err){
             console.log(err)
         }else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets")
-                } else {
-                    res.send("Password is wrong!")
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result){
+                    if(result === true){
+                        res.render("secrets")
+                    }else{
+                        res.send("Password is wrong!")
+                    }
+                })
             } else {
-                res.send("Can't find a user!")
+                res.send("You are not register!")
             }
         }
     })
 })
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    //The Bcrypt Hash Module
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save(function(err){
+            if(err){
+                console.log(err)
+            }else{
+                res.render("secrets")
+            }
+        })
     })
-    newUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets")
-        }
-    })
+    
 })
 
 // LISTEN ////
